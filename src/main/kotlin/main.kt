c932b8f5072084140750c9a7e9de12cd547b0928
braiskiskos <bmartinezparedes@danielcastelao.org>
2021-05-11 17:45:04 +0200

    explicacion

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -13,18 +13,18 @@ fun main(args: Array<String>) {
     println(mayor(6,5))
 
     var tabla2= Array<Int>(10,{it*2})
-    println(tabla2.joinToString())
+    println(tabla2.joinToString()) //con joinToString() recorro ttodo el array para despues poder mostrarlo con println
     var tabla3= Array<Int>(10,{it*3})
     println(tabla3.joinToString())
     var tabla4= Array<Int>(10,{it*4})
     println(tabla4.joinToString())
 
-    fun filtroPorLg(length: Int) {
-        val names = arrayListOf("Adam","Andrew","Chike","Kechi")
-        val filterResultado = names.filter {
-            it.length == length
+    fun filtroPorLg(length: Int) { //pido un numero que sera el tamaño del del nombre o nombres a querer mostrar
+        val names = arrayListOf("Adam","Andrew","Chike","Kechi") // aqui tengo el array con los nombres
+        val filterResultado = names.filter {   // .filter Devuelve una lista que contiene solo elementos que coinciden con el predicado dado.
+            it.length == length // aqui digo que el tamaño a recorrer es igual a al que le dimos  en la funcion
         }
-        println(filterResultado)
+        println(filterResultado) //imprimo el resultado
     }
-    filtroPorLg(6)
+    filtroPorLg(6) //aqui llamo a la funcion para decirle de cuntos carateres tiene que recorrer
 }
