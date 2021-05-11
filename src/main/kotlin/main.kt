902647dcc3af887d4c94d4b0744c0a4a2ad9851e
braiskiskos <bmartinezparedes@danielcastelao.org>
2021-05-11 17:32:32 +0200

    tablas multiplicar

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,5 +10,10 @@ fun main(args: Array<String>) {
     mayor={numero1:Int, numero2:Int-> numero1%2==0 && numero2%2==0}
     println(mayor(6,5))
 
-
+    var tabla2= Array<Int>(10,{it*2})
+    println(tabla2.joinToString())
+    var tabla3= Array<Int>(10,{it*3})
+    println(tabla3.joinToString())
+    var tabla4= Array<Int>(10,{it*4})
+    println(tabla4.joinToString())
 }
