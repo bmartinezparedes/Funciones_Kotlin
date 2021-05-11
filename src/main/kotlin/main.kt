2f563ff0c5e129425c1d203d561d12122263e42e
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-05-11 11:38:17 +0200

    mayor y par

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,4 +5,10 @@ fun main(args: Array<String>) {
     operacion={numero1:Int, numero2:Int-> numero1-numero2}
     println(operacion(6,5))
 
+    var mayor:(Int,Int)-> Boolean={numero1:Int, numero2:Int-> numero1>numero2}
+    println(mayor(6,5))
+    mayor={numero1:Int, numero2:Int-> numero1%2==0 && numero2%2==0}
+    println(mayor(6,5))
+
+
 }
