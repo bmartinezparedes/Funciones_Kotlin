befa460617fde1c6f572365a19717019a2181057
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2021-05-11 11:23:15 +0200

    suma y resta

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    var operacion:(Int,Int)-> Int={numero1:Int, numero2:Int-> numero1+numero2}
+    println(operacion(6,5))
+    operacion={numero1:Int, numero2:Int-> numero1-numero2}
+    println(operacion(6,5))
+
 }
